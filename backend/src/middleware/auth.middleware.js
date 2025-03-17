e26e174b970dae24f693c22790ec8f6bcc6138bf
John Gabriel Gabon <gabonjohngabriel@gmail.com>
2025-03-17 20:06:06 +0800

    Fixed Middleware

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -3,24 +3,26 @@ import User from "../models/user.model.js";
 
 export const protectRoute = async (req, res, next) => {
   try {
-    // Check token in cookies or Authorization header
-    let token = req.cookies.jwt || req.headers.authorization?.split(" ")[1];
+    const token = req.cookies.jwt;
 
     if (!token) {
       return res.status(401).json({ message: "Unauthorized - No Token Provided" });
     }
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
     if (!decoded) {
       return res.status(401).json({ message: "Unauthorized - Invalid Token" });
     }
 
     const user = await User.findById(decoded.userId).select("-password");
+
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
 
     req.user = user;
+
     next();
   } catch (error) {
     console.log("Error in protectRoute middleware: ", error.message);
